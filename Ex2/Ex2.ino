f0a9dde4ab6d1e46f70ba3d11f874aeeb6387430
Sleepy105 <lm.sousa@fe.up.pt>
2017-02-03 22:36:23 +0000

    Initial commit

---
+++ Ex2/Ex2.ino
@@ -0,0 +1,48 @@
+/*
+  IoT Workshop
+  github.com/Sleepy105
+  02/2017
+  Ex2 - Code based on a example of the Adafruit Library for the DHT11 sensor
+*/
+
+#include <DHT.h>
+
+#define DHTPIN 5     // what digital pin we're connected to
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  // Wait a second between measurements.
+  delay(1000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.println(" *C ");
+}
