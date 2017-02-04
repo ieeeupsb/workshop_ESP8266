a450646a6133df05544b8ac6d8f1283454711932
Sleepy105 <lumisousa@gmail.com>
2017-02-04 00:11:41 +0000

    File structure fixes

--- Ex2/Ex2.ino
+++ Ex2/Ex2.ino
@@ -2,40 +2,43 @@
   IoT Workshop
   github.com/Sleepy105
   02/2017
-  Ex2 - Code based on a example of the Adafruit Library for the DHT11 sensor
+  Ex2 - Código baseado num exemplo da Adafruit Library para o sensor DHT11
 */
 
 #include <DHT.h>
 
-#define DHTPIN 5     // what digital pin we're connected to
-#define DHTTYPE DHT11   // DHT 11
+#define DHTPIN 5     // Pino ao qual ligar o sensor DHT11
+#define DHTTYPE DHT11   // Seleciona o modelo do sensor como DHT 11
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(115200);
-  dht.begin();
+  // Este código corre apenas uma vez, quando a placa é inicializada.
+  Serial.begin(115200);     // Inicia a comunicação serial com uma baud rate de 115200.
+  dht.begin();     // Inicia a comunicação com o sensor DHT11
 }
 
 void loop() {
-  // Wait a second between measurements.
+  // Este código corre repetidamente até a energia ser cortada à placa.
   delay(1000);
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Ler a humidade ou temperatura pode demorar até 250 millisegundos!
+  // O sensor é lento e os dados da humidade ou temperatura podem ter um atraso de 2 segundos.
+  // Ler a humidade em %
   float humidity = dht.readHumidity();
-  // Read temperature as Celsius (the default)
+  // Ler a temperatura em Celsius (por defeito).
   float temperature = dht.readTemperature();
 
-  // Check if any reads failed and exit early (to try again).
+  // Verificar se alguma das leituras falhou e tentar novamente.
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  // Compute heat index in Celsius (isFahreheit = false)
+  // Calcula o indice de calor em Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(temperature, humidity, false);
 
+  // Imprime a humidade, temperatura e indice de calor.
   Serial.print("Humidity: ");
   Serial.print(h);
   Serial.print(" %\t");
